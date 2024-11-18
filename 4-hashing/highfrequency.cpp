bdf3274d61450d6438a639116e066b4c825e9d52
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-11-18 07:46:45 +0000

    learning Maps

--- 4-hashing/highfrequency.cpp
+++ 4-hashing/highfrequency.cpp
@@ -10,19 +10,32 @@ int main(){
         cin >> arr[i];
     }
     // pre-compute
-    map <int , int> mpp;
+    map<int , int> mpp;
     for(int i =0; i<n; i++){
         mpp[arr[i]]++;
     }
 
-    int q;
-    cin >> q;
-    while(q--){
-        int number;
-        cin >> number;
-        //fetching
-        cout << mpp[number] << endl;
+    int maxFreq = 0, minFreq = n;
+    int maxEle = 0, minEle = 0;
+
+    for( auto it:mpp){
+        int count = it.second;
+        int element = it.first;
+
+
+        if( count >maxFreq){
+            maxEle = element;
+            maxFreq = count;
+            }
+        
+        if( count < minFreq){
+            minEle = element;
+            minFreq = count;
+        }
     }
+
+    cout << "This is max Frequency element: " << maxEle  << endl;
+    cout << "This is min Frequency element: " << minEle  << endl;
 
     return 0 ;
 }
