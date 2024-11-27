4e6fd42b0492de64f10502de34ad9cce93a2fea4
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-11-27 11:16:23 +0000

    learning array

--- 6-Array/rotate_array.cpp
+++ 6-Array/rotate_array.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void Rotatetoright(int arr[],int n,int k){
+
+void Rotatetoright(int arr[],int n ,int k){
+ 
     if(n==0) return;
     k = k % n;
     if(k>n) return;
@@ -16,17 +18,52 @@ void Rotatetoright(int arr[],int n,int k){
     for(int i = 0; i<k; i++){
         arr[i] = temp[i];
     }
-
+  
 }
+
 
 
 int main(){
     int arr[] = {4,5,6,2,1,12};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int k = 1;
+    int k = 3;
     Rotatetoright(arr,n,k);
     for(int i=0; i<n; i++){
         cout<< arr[i] << " ";
     }
     return 0;
 }
+
+
+// Opimal Approch
+
+
+// void reverse(int arr[], int start, int end){
+
+//     while(start<=end){
+//         int temp = arr[start];
+//         arr[start] = arr[end];
+//         arr[end] = temp;
+//         start++;
+//         end--;
+//     }
+
+// }
+
+// void Rotatetoright(int arr[],int n ,int k){
+//     reverse(arr,0,n-k-1);
+//     reverse(arr,n-k, n-1);
+//     reverse(arr,0, n-1);
+  
+// }
+
+// int main(){
+//     int arr[] = {4,5,6,2,1,12};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     int k = 3;
+//     Rotatetoright(arr,n,k);
+//     for(int i=0; i<n; i++){
+//         cout<< arr[i] << " ";
+//     }
+//     return 0;
+// }
